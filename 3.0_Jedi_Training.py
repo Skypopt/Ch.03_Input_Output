fe99e04ed968d5135413fa5a14ff73ccb12f098b
thompsonlan <llthompson1207@gmail.com>
2021-09-14 12:01:27 -0500

    Finished temp assignment 3.1

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,21 +1,32 @@
 # Sign your name:________________
 # In all the short programs below, do a good job communicating with your end user!
-
+import math
+math.pi
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 
+name= input("what is your name ")
+print("hello", name)
+print("")
 
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
+base= float(input("what is the base "))
+height= float(input("what is the height "))
+print("the area of this triangle is", base*height/2)
+print("")
 
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
-
+radius= float(input("what is the radius "))
+print("the radius is", 2*math.pi*radius)
+print("")
 
 # 4. Ask a user for an integer and then print the square root.
-
+number= float(input("gimme an integer"))
+print("the square root is", number*number)
 
 # 5. Good Star Wars joke: "May the mass times acceleration be with you!" because F=ma. 
 #    Ask the user for mass and acceleration and then print out the Force on one line and "Get it?" on the next.
-
-
-
+mass=float(input("what is the mass"))
+acceleration=float(input("what is the acceleration"))
+print("the force is", mass*acceleration)
+print("get it?")
--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,11 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: ??? Please tell me what this output is! (output is -40)
 
 '''
 
-
-
+f0=float(input("what is the temperature in fahrenheit? "))
+c0=(5/9)*(f0-32)
+print(c0)
 
